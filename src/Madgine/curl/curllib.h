5148d74ffd3c38b46ec06dcdafad978d2bb0b9ed
MadManRises <madmanrises@web.de>
2025-03-30 21:52:33 +0200

    Upgrading libogg

--- src/Madgine/curl/curllib.h
+++ src/Madgine/curl/curllib.h
@@ -1,6 +1,6 @@
 #include "Generic/genericlib.h"
 
-#if defined(Curl_EXPORTS)
+#if defined(MadgineCurl_EXPORTS)
 #    define MADGINE_CURL_EXPORT DLL_EXPORT
 #else
 #    define MADGINE_CURL_EXPORT DLL_IMPORT
