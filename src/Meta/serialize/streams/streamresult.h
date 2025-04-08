6e4bc027b7d1aa16c0f04bedaf9c67bcbfcab658
MadManRises <madmanrises@web.de>
2025-04-08 17:17:57 +0200

    Externalize Shadergen

--- src/Meta/serialize/streams/streamresult.h
+++ src/Meta/serialize/streams/streamresult.h
@@ -57,7 +57,7 @@ namespace Serialize {
         }
     };
 
-#if _MSC_VER && 0
+#if _MSC_VER
 #    define STREAM_ERROR(Type, ...) \
         ::Engine::Serialize::StreamResultBuilder { Type, __VA_ARGS__, __FILE__, __LINE__ }
 #else
