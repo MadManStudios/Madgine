560bd628849a37986641749b53de0d43ae6695b6
mschutz <martin.schutz@iec-collective.com>
2025-06-17 14:58:54 +0200

    Adding APK cpack target

--- cmake/platform/android.cmake
+++ cmake/platform/android.cmake
@@ -6,7 +6,7 @@ include (Plugins)
 
 if (ANDROID)
 
-	set (Android_List_dir ${CMAKE_CURRENT_LIST_DIR} CACHE INTERNAL "")
+	set (Android_List_dir ${workspace_file_dir}/cpack CACHE INTERNAL "")
 
 	set (ANDROID_DEPENDENCIES "" CACHE INTERNAL "")
 	set (ANDROID_RESOURCES "" CACHE INTERNAL "")
@@ -17,25 +17,6 @@ if (ANDROID)
 	endif()
 
 	MESSAGE(STATUS "Targeting Android-SDK version: ${ANDROID_PLATFORM_LEVEL}")
-
-	set (MADGINE_FORCE_DATA_COLLECT ON CACHE INTERNAL "")
-
-	file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/gradle)
-
-	if (CMAKE_HOST_WIN32)	
-		add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/gradle/gradlew
-			COMMAND gradle init
-			COMMAND gradle wrapper --gradle-version=7.3.3 --distribution-type=all
-			COMMAND gradle --stop
-			WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/gradle)
-	else()
-		add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/gradle/gradlew
-			COMMAND gradle init
-			COMMAND gradle wrapper --gradle-version=7.3.3 --distribution-type=all			
-			WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/gradle)
-	endif()
-
-	add_custom_target(gradlew DEPENDS ${CMAKE_BINARY_DIR}/gradle/gradlew)
 
 	macro(add_workspace_application target)
 
@@ -64,49 +45,45 @@ if (ANDROID)
 		configure_file(${Android_List_dir}/android/AndroidManifest.xml.in AndroidManifest.xml.in @ONLY)
 		configure_file(${Android_List_dir}/android/settings.gradle.in settings.gradle @ONLY)
 		file(GENERATE OUTPUT AndroidManifest.xml INPUT ${CMAKE_CURRENT_BINARY_DIR}/AndroidManifest.xml.in)
-		#configure_file(${Android_List_dir}/android/launch.vs.json.in ${CMAKE_SOURCE_DIR}/.vs/launch.vs.json @ONLY)
-		#configure_file(${Android_List_dir}/android/debug_apk.bat.in debug_${target}_apk.bat @ONLY)
-		#configure_file(${Android_List_dir}/android/launch_apk.sh.in launch_${target}_apk.sh @ONLY)
-		#configure_file(${Android_List_dir}/android/gdbcommands.in gdbcommands_${target}_apk @ONLY)
-		#configure_file(${Android_List_dir}/android/launch_jdb.bat.in launch_jdb.bat @ONLY)
+		
+		install(FILES ${CMAKE_CURRENT_BINARY_DIR}/build.gradle
+					${CMAKE_CURRENT_BINARY_DIR}/local.properties
+					${CMAKE_CURRENT_BINARY_DIR}/gradle.properties
+					${CMAKE_CURRENT_BINARY_DIR}/settings.gradle
+					${CMAKE_CURRENT_BINARY_DIR}/AndroidManifest.xml
+				DESTINATION .
+				COMPONENT ${target})
 
 		add_library(${target} SHARED ${ARGN})
-
-		set_target_properties(${target} PROPERTIES
-		LIBRARY_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/bin/${CMAKE_ANDROID_ARCH_ABI}
-		LIBRARY_OUTPUT_DIRECTORY_DEBUG ${CMAKE_CURRENT_BINARY_DIR}/bin/${CMAKE_ANDROID_ARCH_ABI}
-		LIBRARY_OUTPUT_DIRECTORY_RELEASE ${CMAKE_CURRENT_BINARY_DIR}/bin/${CMAKE_ANDROID_ARCH_ABI}
-		LIBRARY_OUTPUT_DIRECTORY_RELWITHDEBINFO ${CMAKE_CURRENT_BINARY_DIR}/bin/${CMAKE_ANDROID_ARCH_ABI})
 
 		if (NOT MODULES_ENABLE_PLUGINS)
 			patch_toplevel_target(${target})
 		endif()
 
-		add_executable(${target}_apk android/dummy.cpp)
-
-		target_link_libraries(${target}_apk PRIVATE ${target})
-	
-		if (CMAKE_HOST_WIN32)	
-			add_custom_command(
-				TARGET ${target}
-				POST_BUILD				
-				COMMAND ${CMAKE_BINARY_DIR}/gradle/gradlew assembleDebug
-				COMMAND ${CMAKE_BINARY_DIR}/gradle/gradlew --stop
-				COMMENT "Build APK - ${target}"			
-				BYPRODUCTS apk/${target}-debug.apk
-			)	
-		else()
-			add_custom_command(
-				TARGET ${target}
-				POST_BUILD				
-				COMMAND ${CMAKE_BINARY_DIR}/gradle/gradlew assembleDebug
-				COMMENT "Build APK - ${target}"			
-				BYPRODUCTS apk/${target}-debug.apk
-			)		
-		endif()
-
-		add_dependencies(${target} gradlew)
-
 	endmacro(add_workspace_application)
+
+	function(install_to_workspace name)
+
+		set(options)
+		set(oneValueArgs)
+		set(multiValueArgs TARGETS)
+		cmake_parse_arguments(OPTIONS "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+		install(
+			TARGETS ${OPTIONS_TARGETS} 
+			LIBRARY DESTINATION bin/${CMAKE_ANDROID_ARCH_ABI} COMPONENT ${name}
+			BUNDLE DESTINATION App COMPONENT ${name}
+		)
+
+		foreach(target ${OPTIONS_TARGETS})
+			get_target_property(TARGET_SOURCE_DIR ${target} SOURCE_DIR)
+			if (EXISTS ${TARGET_SOURCE_DIR}/data)
+				install(DIRECTORY ${TARGET_SOURCE_DIR}/data DESTINATION . COMPONENT ${name})
+			endif()
+
+			#target_include_directories(${target} INTERFACE $<INSTALL_INTERFACE:$<INSTALL_PREFIX>/${target}/include>)
+		endforeach()
+
+	endfunction(install_to_workspace)
 
 endif()
