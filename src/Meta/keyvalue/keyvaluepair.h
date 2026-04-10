6369a678d9b902d3a10a140d3a03c16ba677211a
Martin Schütz <madmanrises@web.de>
2026-04-10 16:07:41 +0200

    Make KeyValuePair decomposable manually

--- src/Meta/keyvalue/keyvaluepair.h
+++ src/Meta/keyvalue/keyvaluepair.h
@@ -5,8 +5,33 @@
 namespace Engine {
 
 struct KeyValuePair {
+
+    template <size_t I>
+    ValueType &get() &
+    {
+        if constexpr (I == 0)
+            return mKey;
+        else if constexpr (I == 1)
+            return mValue;
+        else
+            static_assert(dependent_bool<size_t, (I < 2)>::value, "Index out of bounds for KeyValuePair");
+    }
+
     ValueType mKey;
     ValueType mValue;
 };
+
+}
+
+namespace std {
+template <>
+struct tuple_size<Engine::KeyValuePair> : std::integral_constant<size_t, 2> { };
+
+/* template <typename V1, typename... V>
+struct tuple_element<0, Engine::Execution::ValueStorageImpl<V1, V...>> {
+    using type = V1;
+};
+template <size_t I, typename V1, typename... V>
+struct tuple_element<I, Engine::Execution::ValueStorageImpl<V1, V...>> : tuple_element<I - 1, Engine::Execution::ValueStorageImpl<V...>> { };*/
 
 }
