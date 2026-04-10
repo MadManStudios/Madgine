f126ba73b7778e766ba12937e49d7c8368b55a6c
Martin Schütz <madmanrises@web.de>
2026-04-10 17:37:06 +0200

    Add const get() overload to KeyValuePair

--- src/Meta/keyvalue/keyvaluepair.h
+++ src/Meta/keyvalue/keyvaluepair.h
@@ -7,7 +7,40 @@ namespace Engine {
 struct KeyValuePair {
 
     template <size_t I>
+    ValueType &&get() &&
+    {
+        if constexpr (I == 0)
+            return std::move(mKey);
+        else if constexpr (I == 1)
+            return std::move(mValue);
+        else
+            static_assert(dependent_bool<size_t, (I < 2)>::value, "Index out of bounds for KeyValuePair");
+    }
+
+    template <size_t I>
+    const ValueType &&get() const &&
+    {
+        if constexpr (I == 0)
+            return std::move(mKey);
+        else if constexpr (I == 1)
+            return std::move(mValue);
+        else
+            static_assert(dependent_bool<size_t, (I < 2)>::value, "Index out of bounds for KeyValuePair");
+    }
+
+    template <size_t I>
     ValueType &get() &
+    {
+        if constexpr (I == 0)
+            return mKey;
+        else if constexpr (I == 1)
+            return mValue;
+        else
+            static_assert(dependent_bool<size_t, (I < 2)>::value, "Index out of bounds for KeyValuePair");
+    }
+
+    template <size_t I>
+    const ValueType &get() const &
     {
         if constexpr (I == 0)
             return mKey;
@@ -27,11 +60,9 @@ namespace std {
 template <>
 struct tuple_size<Engine::KeyValuePair> : std::integral_constant<size_t, 2> { };
 
-/* template <typename V1, typename... V>
-struct tuple_element<0, Engine::Execution::ValueStorageImpl<V1, V...>> {
-    using type = V1;
+template <size_t I>
+struct tuple_element<I, Engine::KeyValuePair> {
+    using type = Engine::ValueType;
 };
-template <size_t I, typename V1, typename... V>
-struct tuple_element<I, Engine::Execution::ValueStorageImpl<V1, V...>> : tuple_element<I - 1, Engine::Execution::ValueStorageImpl<V...>> { };*/
 
 }
